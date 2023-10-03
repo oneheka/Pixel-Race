295e5e320d526068c3565b89bfcedaeac6df310e
unfeel <un7eel@yandex.ru>
2023-10-03 23:13:10 +1200

    heka udali discord

--- src/main.py
+++ src/main.py
@@ -4,7 +4,9 @@ from pages.MainPage import MainPage
 class Pygame:
     screen = pygame.display.set_mode((360, 660))
     pygame.display.set_caption('Niako Game')
-    icon = pygame.image.load('assets/images/icon.png')
+    icon = pygame.image.load('assets/images/icon.png').convert_alpha()
+    background = pygame.image.load('assets/images/bg.png').convert_alpha()
+    background_y = 0
     state = 'Menu'
     
 
@@ -12,16 +14,24 @@ class Pygame:
         pygame.font.init()
         pygame.init()
         pygame.display.set_icon(self.icon)
+        self.screen.blit(self.background, ((0, self.background_y)))
 
         while(bool(self.state)):
             for event in pygame.event.get():
                 if(event.type == pygame.QUIT):
                     pygame.quit()
-            
+
             if(self.state == 'Menu'):
-                MainPage(self.screen).render()
+                MainPage(self.screen, self.background_y).render()
+            
+            self.screen.blit(self.background, ((0, self.background_y + 1000)))
+            self.background_y -= 2
+            if (self.background_y == -1000):
+                self.background_y = 0
+
             
 
             pygame.display.update()
+            pygame.time.Clock().tick(300)
 
 Pygame().build()
--- src/pages/MainPage.py
+++ src/pages/MainPage.py
@@ -1,8 +1,16 @@
 import pygame
 
 class MainPage:
-    def __init__(self, main):
+    def __init__(self, main, bg_y):
         self.main = main
+        self.bg_y = bg_y
 
     def render(self):
-        self.main.fill('red')
+        buttonPlay = pygame.image.load('assets/images/buttons/playButton.png').convert_alpha()
+        buttonSkins = pygame.image.load('assets/images/buttons/skinsButton.png').convert_alpha()
+        darker_bg = pygame.image.load('assets/images/darker_bg.png').convert_alpha()
+        
+        self.main.blit(darker_bg, ((0, 0)))
+        self.main.blit(buttonPlay, ((90, 261)))
+        self.main.blit(buttonSkins, ((90, 334)))
+        
