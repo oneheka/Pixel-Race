e1359a7e8101289594734c423d4a97a4c2793410
unfeel <un7eel@yandex.ru>
2023-10-03 22:13:00 +1200

    heka idi nahuy

--- src/main.py
+++ src/main.py
@@ -1,18 +1,21 @@
 import pygame
 
 class Pygame:
-    screen = pygame.display.set_mode((512, 512))
+    screen = pygame.display.set_mode((360, 660))
+    pygame.display.set_caption('Niako Game')
+    icon = pygame.image.load('assets/images/icon.png')
     state = 'Menu'
+    
 
     def build(self):
         pygame.font.init()
         pygame.init()
-
+        pygame.display.set_icon(self.icon)
         while(bool(self.state)):
             for event in pygame.event.get():
                 if(event.type == pygame.QUIT):
                     pygame.quit()
 
-            pygame.display.flip()
+            pygame.display.update()
 
 Pygame().build()
