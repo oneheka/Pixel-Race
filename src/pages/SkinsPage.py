4d1e7a43024343b91e31f1ea98eec4ed25c46cef
MAKDAC2020 <76689339+HekaHub@users.noreply.github.com>
2023-10-05 11:14:23 +0700

    уебище пейдж не меняет

--- src/pages/SkinsPage.py
+++ src/pages/SkinsPage.py
@@ -1,25 +1,63 @@
 import pygame
+import json
 
 class SkinsPage:
     def __init__(self, game):
         self.game = game
         self.mouse = pygame.mouse.get_pos()
+        self.page = 0
+        with open('src/config.json') as f:
+            file_content = f.read()
+            self.config = json.loads(file_content)
+
 
     def render(self, clicked):
         self.game.screen.blit(self.game.util.skins[0], ((152, self.game.eronary_y + 1000)))
         self.game.screen.blit(self.game.util.skins[1], ((28, self.game.walpuper_y + 1000)))
         self.game.screen.blit(self.game.util.skins[2], ((276, self.game.aqua_y + 1000)))
         self.game.screen.blit(self.game.util.darkerBackground, ((0, 0)))
+
+        skins = self.config['skins']
+
         self.game.screen.blit(self.game.util.skins_page[0], ((101, 200)))
         self.game.screen.blit(self.game.util.skins_page[1], ((132, 395)))
-        self.game.screen.blit(self.game.util.skins_page[2], ((150, 265)))
-        self.game.screen.blit(self.game.util.skins_page[3], ((186, 236)))
-        self.game.screen.blit(self.game.util.skins_page[5], ((296, 306)))
-        self.game.screen.blit(self.game.util.skins_page[4], ((16, 306)))
-        self.game.screen.blit(self.game.util.skins_page[6], ((149, 238)))
-        self.game.screen.blit(self.game.util.skins_page[7], ((132, 364)))
+        self.game.screen.blit(self.game.util.coin, ((186, 236)))
+        self.game.screen.blit(
+            self.game.util.getImage(skins[self.page]['name']),
+            ((150, 265))
+        )
+        self.game.screen.blit(
+            self.game.util.mainfont.render(
+                str(skins[self.page]['cost']), False, 'white'
+            ), ((149, 238))
+        )
+        self.game.screen.blit(
+            self.game.util.mainfont.render(
+                skins[self.page]['name'], False, 'white'
+            ), ((132, 364))
+        )
+
+        left = self.game.screen.blit(self.game.util.skins_page[2], ((16, 306)))
+        if left.collidepoint(self.mouse) and clicked:
+            self.left()
+
+        right = self.game.screen.blit(self.game.util.skins_page[3], ((296, 306)))
+        if right.collidepoint(self.mouse) and clicked:
+            self.right()
 
         settings = self.game.screen.blit(self.game.util.settings[1], ((305, 15)))
         if settings.collidepoint(self.mouse) and clicked:
             if(self.game.state != "Menu"):
                 self.game.state = 'Menu'
+
+    def right(self):
+        if(self.page + 1 >= len(self.config['skins'])):
+            self.page = 0
+        else:
+            self.page += 1
+
+    def left(self):
+        if(0 > self.page - 1):
+            self.page = len(self.config['skins']) - 1
+        else:
+            self.page -= 1
--- src/util.py
+++ src/util.py
@@ -12,17 +12,13 @@ class Util:
         self.settingsBackground = pygame.image.load('assets/images/settings_bg.png').convert_alpha()
         self.darkerBackground = pygame.image.load('assets/images/darker_bg.png').convert_alpha()
         self.coins = pygame.image.load('assets/images/coins.png').convert_alpha()
+        self.coin = pygame.image.load('assets/images/coin.png').convert_alpha()
         self.record = pygame.image.load('assets/images/record.png').convert_alpha()
         self.skins_page = [
                 pygame.image.load('assets/images/skins_plate.png').convert_alpha(),
                 pygame.image.load('assets/images/buttons/buyButton.png').convert_alpha(),
-                pygame.image.load('assets/images/skins/walpuperShop.png').convert_alpha(),
-                pygame.image.load('assets/images/coin.png').convert_alpha(),
                 pygame.image.load('assets/images/buttons/leftArrow.png').convert_alpha(),
-                pygame.image.load('assets/images/buttons/rightArrow.png').convert_alpha(),
-                self.mainfont.render('100', False, 'white'),
-                self.mainfont.render('Walpuper', False, 'white')
-
+                pygame.image.load('assets/images/buttons/rightArrow.png').convert_alpha()
         ]
         self.settings = [
             pygame.image.load('assets/images/settings.png').convert_alpha(),
@@ -38,3 +34,11 @@ class Util:
             pygame.image.load('assets/images/skins/walpuperCar.png').convert_alpha(),
             pygame.image.load('assets/images/skins/aquaCar.png').convert_alpha(),
         ]
+    
+    def getImage(self, name):
+        if(name == 'Eronary'):
+            return self.skins[0]
+        elif(name == 'Walpuper'):
+            return self.skins[1]
+        else:
+            return self.skins[2]
